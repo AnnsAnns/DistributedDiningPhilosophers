a1ea65fa9003796e75f95a5416b13bda61b8a04b
AnnsAnn <git@annsann.eu>
2025-01-30 09:23:17 +0100

    States Tests

--- shared_menu/src/states.rs
+++ shared_menu/src/states.rs
@@ -50,3 +50,49 @@ impl States {
         bincode::serialize(self).unwrap()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::States;
+    use bincode;
+
+    #[test]
+    fn test_is_clean() {
+        assert!(States::CutleryClean(true).is_clean());
+        assert!(States::CutleryClean(false).is_clean());
+        assert!(!States::CutleryDirty(true).is_clean());
+        assert!(!States::CutleryDirty(false).is_clean());
+    }
+
+    #[test]
+    fn test_is_dirty() {
+        assert!(!States::CutleryClean(true).is_dirty());
+        assert!(!States::CutleryClean(false).is_dirty());
+        assert!(States::CutleryDirty(true).is_dirty());
+        assert!(States::CutleryDirty(false).is_dirty());
+    }
+
+    #[test]
+    fn test_is_used() {
+        assert!(States::CutleryClean(true).is_used());
+        assert!(!States::CutleryClean(false).is_used());
+        assert!(States::CutleryDirty(true).is_used());
+        assert!(!States::CutleryDirty(false).is_used());
+    }
+
+    #[test]
+    fn test_serialization() {
+        let state = States::PhilosopherThinking;
+        let bytes = state.to_bytes();
+        let deserialized_state = States::from_bytes(bytes);
+        assert_eq!(state, deserialized_state);
+    }
+
+    #[test]
+    fn test_deserialization() {
+        let state = States::WaiterActive;
+        let bytes = bincode::serialize(&state).unwrap();
+        let deserialized_state: States = bincode::deserialize(&bytes).unwrap();
+        assert_eq!(state, deserialized_state);
+    }
+}
